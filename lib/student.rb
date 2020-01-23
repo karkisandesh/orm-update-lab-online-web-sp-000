68f03e5da65af2db826e36ff180f1e936b690520
Sandesh Karki <karkisandesh@hotmail.com>
2020-01-23 17:15:14 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -60,9 +60,8 @@ class Student
  def self.find_by_name(name)
     sql = <<-SQL
       SELECT *
-      FROM songs
+      FROM students
       WHERE name = ?
-      LIMIT 1
     SQL
  
     DB[:conn].execute(sql, name).map do |row|
