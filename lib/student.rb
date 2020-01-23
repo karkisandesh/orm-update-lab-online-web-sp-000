64739dda13597198f164998f1288d092019344aa
Sandesh Karki <karkisandesh@hotmail.com>
2020-01-23 17:11:15 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -53,5 +53,12 @@ class Student
     
   end
   
+# def self.new_from_db(row)
+#   new_song = self.new  # self.new is the same as running Song.new
+#   new_song.id = row[0]
+#   new_song.name =  row[1]
+#   new_song.length = row[2]
+#   new_song  # return the newly created instance
+# end
 
 end
