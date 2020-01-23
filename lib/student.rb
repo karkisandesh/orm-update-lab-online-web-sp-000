ae10377a6d01ce084c333a9fa64317863d101460
Sandesh Karki <karkisandesh@hotmail.com>
2020-01-23 17:23:09 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -8,7 +8,7 @@ class Student
  attr_accessor :name, :grade
  attr_reader :id
  
- def initialize(id=nil, name, grade)
+ def initialize(id, name, grade)
    @id = id 
    @name = name 
    @grade = grade
